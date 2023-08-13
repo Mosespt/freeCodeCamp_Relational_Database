0c6d3c6c4b35a242dbc3fd7bd9e4a291cfcf18c7
Odeyemi Tolulope Moses <tolulopemoses@aol.com>
2023-08-14 00:47:54 +0200

    Create bike-shop.sh

---
+++ Learn Bash and SQL by Building a Bike Rental Shop/bike-shop.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
+
+echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
+
+MAIN_MENU() {
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+  
+  echo How may I help you?
+  # Question 
+  echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
+  read MAIN_MENU_SELECTION
+}
+RENT_MENU() {
+  # get available bikes
+  AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available=true ORDER BY bike_id")
+
+  # if no bikes available
+  if [[ -z $AVAILABLE_BIKES ]]
+  then
+    # send to main menu
+    MAIN_MENU "Sorry, we don't have any bikes available right now."
+  else
+    # display available bikes
+    echo -e "\nHere are the bikes we have available:"
+    echo "$AVAILABLE_BIKES" | while read BIKE_ID BAR TYPE BAR SIZE
+    do
+      echo "$BIKE_ID $TYPE $SIZE"
+    done 
+
+    # ask for bike to rent
+    # if input is not a number
+    # send to main menu
+  fi
+}
+RETURN_MENU() {
+  echo Return Menu
+}
+EXIT() {
+  echo -e "\nThank you for stopping in.\n"
+}
+
+MAIN_MENU
+
+case $MAIN_MENU_SELECTION in
+  1) RENT_MENU ;;
+  2) RETURN_MENU ;;
+  3) EXIT ;;
+  *) MAIN_MENU "Please enter a valid option." ;;
+esac
